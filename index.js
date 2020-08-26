7ed25c0a484050b7214119279555d3a1df7db732
Dave Enriquez <const.dave@gmail.com>
2020-08-25 22:12:56 -0500

    fixed cors and logging the formData

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const port = process.env.PORT || 3000;
 const app = express();
+const addBookRouter = require('../server/routes/addBook')
+const volleyball = require('volleyball')
+const cors = require('cors')
 
+app.use(cors());
 app.use(express.json())
+app.use(addBookRouter)
+app.use(volleyball)
 
 
 app.listen(port, () => {
